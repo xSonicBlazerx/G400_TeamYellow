763ca78ab63118b758737e1105e8c97d053a05cc
Daniel Barnes <barndajo@indiana.edu>
2017-08-24 20:37:25 -0400

    Created Player Object and Player Script

---
+++ Gather N Sell/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public int LumberSupply;
+	public int BerrySupply;
+	public int CoalSupply;
+	public int MoneySupply;
+	public int TimeRemaining;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
