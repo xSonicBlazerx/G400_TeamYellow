a47f3f23fc93fad3d151c996bf149077f09386fe
Daniel Barnes <barndajo@indiana.edu>
2017-08-26 20:47:37 -0400

    Added player movement code to player script - player can now move the player object left and right in the play area. Added Gather Manager Script, player can now gather resources with this script, resource must be properly tagged and a check added to code for new resources past those in MVP.

---
+++ Gather N Sell/Assets/Scripts/GatherManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatherManager : MonoBehaviour {
+	public float gatherTimer = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		Debug.Log ("Item Collides!");
+		if(other.gameObject.CompareTag("Player")){
+			if (Input.GetButton ("Gather")) {
+				gatherTimer -= Time.deltaTime;
+				Debug.Log (gatherTimer);
+			}
+			if (gatherTimer < 0) {
+				Destroy (gameObject);
+				GameObject player = GameObject.Find ("Player");
+				var playerScript = player.GetComponent<Player>();
+				if (this.gameObject.CompareTag ("Tree")) {
+					Debug.Log (playerScript.LumberSupply);
+					playerScript.LumberSupply++;
+					Debug.Log (playerScript.LumberSupply);
+				} else if (this.gameObject.CompareTag ("Berry")) {
+					Debug.Log (playerScript.BerrySupply);
+					playerScript.BerrySupply++;
+					Debug.Log (playerScript.BerrySupply);
+				} else if (this.gameObject.CompareTag ("Coal")) {
+					Debug.Log (playerScript.CoalSupply);
+					playerScript.CoalSupply++;
+					Debug.Log (playerScript.CoalSupply);
+				}
+				Debug.Log ("Item Gathered!");
+			}
+		}
+	}
+}
--- Gather N Sell/Assets/Scripts/Player.cs
+++ Gather N Sell/Assets/Scripts/Player.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public int LumberSupply;
-	public int BerrySupply;
-	public int CoalSupply;
-	public int MoneySupply;
+	public int LumberSupply = 0;
+	public int BerrySupply = 0;
+	public int CoalSupply = 0;
+	public int MoneySupply = 0;
 	public int TimeRemaining;
+	public float hForce = 200f;
+	public float maxSpeed = 2f;
+
+	private Rigidbody2D rb2d;
 	// Use this for initialization
 	void Start () {
-		
+		rb2d = GetComponent<Rigidbody2D> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis ("Horizontal");
+
+		if (h * rb2d.velocity.x < maxSpeed) {
+			rb2d.AddForce (Vector2.right * h * hForce);
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed) {
+			rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+		}
 	}
 }
